44e205a5c2461d40a99aaed502fd9c3aec1f07f2
KurekMartin <martinkurek.dev@gmail.com>
2024-06-09 20:37:50 +0200

    add class for team sorting

---
+++ TeamSorting/ViewModel/TeamSorting.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamSorting.ViewModel;
+
+public class TeamSorting
+{
+    public MembersData MembersData = new();
+    public TeamsCollection TeamsCollection = new();
+
+    public void SortMembersIntoTeams()
+    {
+        var teamCount = TeamsCollection.Teams.Count;
+        //TODO
+    }
+}
