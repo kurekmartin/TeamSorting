9e3cf21cf02a380d149ff387ae846c5636a83104
KurekMartin <martinkurek.dev@gmail.com>
2024-07-18 21:54:42 +0200

    add sorter interface, evolution sorter basic structure

---
+++ TeamSorting/Sorting/EvolutionSorter.cs
@@ -0,0 +1,36 @@
+﻿using TeamSorting.Models;
+
+namespace TeamSorting.Sorting;
+
+public class EvolutionSorter : ISorter
+{
+    public List<Team> Sort(List<Member> members, int numberOfTeams)
+    {
+        //TODO
+        throw new NotImplementedException();
+    }
+
+    private List<Team> CreateRandomCombination(List<Member> members)
+    {
+        //TODO
+        throw new NotImplementedException();
+    }
+
+    private double TeamScore(List<Member> members)
+    {
+        //TODO
+        throw new NotImplementedException();
+    }
+
+    private Tuple<List<Member>, List<Member>> Cross(List<Member> parent1, List<Member> parent2)
+    {
+        //TODO
+        throw new NotImplementedException();
+    }
+
+    private List<Member> Mutate(List<Member> members)
+    {
+        //TODO
+        throw new NotImplementedException();
+    }
+}
---
+++ TeamSorting/Sorting/ISorter.cs
@@ -0,0 +1,8 @@
+﻿using TeamSorting.Models;
+
+namespace TeamSorting.Sorting;
+
+public interface ISorter
+{
+    public List<Team> Sort(List<Member> members, int numberOfTeams);
+}
