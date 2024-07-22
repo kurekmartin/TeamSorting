8f6d008f8d12d4c81596613d84e244227a8f4eb2
KurekMartin <martinkurek.dev@gmail.com>
2024-07-22 15:13:51 +0200

    change team validity to icon

---
+++ TeamSorting/Converters/TeamValidityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+using TeamSorting.Models;
+
+namespace TeamSorting.Converters;
+
+public class TeamValidityConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is bool)
+        {
+            return value switch
+            {
+                true => "mdi-check-circle",
+                false => "mdi-close-circle",
+                _ => "mdi-help"
+            };
+        }
+
+        return "mdi-help";
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
