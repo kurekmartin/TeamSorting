330f35fb5c5dacf36ce31924b1451baa33cc0d45
KurekMartin <martinkurek.dev@gmail.com>
2024-07-23 10:39:26 +0200

    add program version

--- TeamSorting/ViewModels/MainWindowViewModel.cs
+++ TeamSorting/ViewModels/MainWindowViewModel.cs
@@ -5,11 +5,15 @@ namespace TeamSorting.ViewModels;
 public class MainWindowViewModel(TeamsViewModel teamsViewModel, InputViewModel inputViewModel) : ViewModelBase
 {
     private ViewModelBase _contentViewModel = inputViewModel;
+
     public ViewModelBase ContentViewModel
     {
         get => _contentViewModel;
         private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
     }
+
+    public string Version =>
+        $"v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "?.?.?"}";
 
     public void SwitchToTeamsView()
     {
