8a70939830b6f6560e88fa4b0584cc448bb991dc
KurekMartin <martinkurek.dev@gmail.com>
2024-09-07 17:15:44 +0200

    cache parsed value of discipline record

--- TeamSorting/Models/DisciplineRecord.cs
+++ TeamSorting/Models/DisciplineRecord.cs
@@ -7,22 +7,43 @@ public class DisciplineRecord(Member member, DisciplineInfo disciplineInfo, stri
 {
     public Member Member { get; } = member;
     public DisciplineInfo DisciplineInfo { get; } = disciplineInfo;
-    public string RawValue { get; set; } = rawValue;
+
+    private string _rawValue = rawValue;
+    public string RawValue
+    {
+        get => _rawValue;
+        set
+        {
+            _rawValue = value;
+            _isValueUpdated = false;
+        }
+    }
+
+    private bool _isValueUpdated;
+    private object? _value;
 
     public object Value
     {
         get
         {
-            return DisciplineInfo.DataType switch
+            if (_isValueUpdated && _value is not null)
+            {
+                return _value;
+            }
+
+            _value = DisciplineInfo.DataType switch
             {
                 DisciplineDataType.Time => string.IsNullOrWhiteSpace(RawValue)
                     ? TimeSpan.Zero
                     : TimeSpan.Parse(RawValue, CultureInfo.InvariantCulture),
                 DisciplineDataType.Number => string.IsNullOrWhiteSpace(RawValue)
                     ? 0d
-                    : double.Parse(RawValue,CultureInfo.InvariantCulture),
+                    : double.Parse(RawValue, CultureInfo.InvariantCulture),
                 _ => throw new FormatException()
             };
+
+            _isValueUpdated = true;
+            return _value;
         }
     }
 
