c83cdf3f8a00364fec222f2e536521de70ce1d7b
KurekMartin <martinkurek.dev@gmail.com>
2025-04-15 19:34:43 +0200

    initial support for displaying DisciplineRecord value based on data type

---
+++ TeamSorting/Converters/DisciplineRecordValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace TeamSorting.Converters;
+
+public class DisciplineRecordValueConverter: IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is TimeSpan timeSpan)
+        {
+            return timeSpan.ToString($@"hh\:mm\:ss\{CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator}f");
+        }
+        return value?.ToString();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
