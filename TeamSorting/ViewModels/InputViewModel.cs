0fe78e738583e62a41d45b8d9c482c6d2ec6322c
KurekMartin <martinkurek.dev@gmail.com>
2025-05-08 17:06:21 +0200

    Change the display of input data from DataGrid to TreeDataGrid

--- TeamSorting/ViewModels/InputViewModel.cs
+++ TeamSorting/ViewModels/InputViewModel.cs
@@ -1,10 +1,28 @@
-﻿using TeamSorting.Enums;
+﻿using System.Collections;
+using System.Collections.Specialized;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Controls;
+using TeamSorting.Enums;
+using TeamSorting.Models;
+using Avalonia.Controls.Models.TreeDataGrid;
+using Avalonia.Controls.Templates;
+using Avalonia.Data;
+using Avalonia.Interactivity;
+using Avalonia.Layout;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
+using Projektanker.Icons.Avalonia;
+using TeamSorting.Controls;
+using TeamSorting.Converters;
+using TeamSorting.Lang;
 
 namespace TeamSorting.ViewModels;
 
-public class InputViewModel(Data data) : ViewModelBase
+public class InputViewModel : ViewModelBase
 {
-    public Data Data { get; } = data;
+    public Data Data { get; }
+    public FlatTreeDataGridSource<Member> TreeDataGridSource { get; }
     public int NumberOfTeams { get; set; } = 2;
     private string _newMemberName = string.Empty;
 
@@ -13,7 +31,7 @@ public class InputViewModel(Data data) : ViewModelBase
         get => _newMemberName;
         set => SetProperty(ref _newMemberName, value);
     }
-    
+
     private string _newDisciplineName = string.Empty;
 
     public string NewDisciplineName
@@ -24,4 +42,243 @@ public class InputViewModel(Data data) : ViewModelBase
 
     public static Array DisciplineDataTypes => Enum.GetValues(typeof(DisciplineDataType));
     public static Array SortOrder => Enum.GetValues(typeof(SortOrder));
+
+    public InputViewModel(Data data)
+    {
+        Data = data;
+        data.Disciplines.CollectionChanged += DisciplinesOnCollectionChanged;
+        TreeDataGridSource = new FlatTreeDataGridSource<Member>(data.Members)
+        {
+            Columns =
+            {
+                new TemplateColumn<Member>(null, "RemoveMemberCell", null, GridLength.Auto),
+                new TextColumn<Member, string>(Resources.InputView_DataGrid_ColumnHeader_Name,
+                    member => member.Name,
+                    (member, s) => member.Name = s ?? member.Name,
+                    GridLength.Auto,
+                    new TextColumnOptions<Member>
+                    {
+                        BeginEditGestures = BeginEditGestures.Tap
+                    }),
+                new TemplateColumn<Member>(Resources.InputView_DataGrid_ColumnHeader_With, "WithCell", null, GridLength.Auto),
+                new TemplateColumn<Member>(Resources.InputView_DataGrid_ColumnHeader_NotWith, "NotWithCell", null, GridLength.Auto)
+            }
+        };
+    }
+
+    private void DisciplinesOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        switch (e.Action)
+        {
+            case NotifyCollectionChangedAction.Add:
+                AddDisciplineColumns(e.NewItems);
+                break;
+            case NotifyCollectionChangedAction.Remove:
+                RemoveDisciplineColumns(e.OldItems);
+                break;
+        }
+    }
+
+    private void AddDisciplineColumns(IList? disciplines)
+    {
+        if (disciplines is null)
+        {
+            return;
+        }
+
+        foreach (object disciplineObject in disciplines)
+        {
+            if (disciplineObject is DisciplineInfo discipline)
+            {
+                AddDisciplineColumn(discipline);
+            }
+        }
+    }
+
+    private void AddDisciplineColumn(DisciplineInfo discipline)
+    {
+        var column = new TemplateColumn<Member>(
+            CreateDisciplineColumnHeader(discipline),
+            GetDisciplineCellTemplate(discipline),
+            GetDisciplineEditTemplate(discipline),
+            GridLength.Auto,
+            new TemplateColumnOptions<Member>
+            {
+                CanUserSortColumn = false,
+                BeginEditGestures = BeginEditGestures.Tap,
+            })
+        {
+            Tag = CreateDisciplineColumnTag(discipline)
+        };
+
+        TreeDataGridSource.Columns.Add(column);
+    }
+
+    private static IDataTemplate GetDisciplineCellTemplate(DisciplineInfo discipline)
+    {
+        return new FuncDataTemplate<Member?>((member, _) =>
+            new TextBlock
+            {
+                Text = (string)(new DisciplineRecordValueConverter().Convert(member?.Records[discipline.Id].Value, typeof(string), null, CultureInfo.CurrentCulture) ?? string.Empty),
+                HorizontalAlignment = HorizontalAlignment.Stretch,
+                VerticalAlignment = VerticalAlignment.Center,
+                Padding = new Thickness(4, 2),
+                TextAlignment = TextAlignment.Right,
+            }
+        );
+    }
+
+    private IDataTemplate GetDisciplineEditTemplate(DisciplineInfo discipline)
+    {
+        FuncDataTemplate<Member> template = discipline.DataType switch
+        {
+            DisciplineDataType.Number => new FuncDataTemplate<Member>((_, _) => new NumericUpDown
+            {
+                [!NumericUpDown.ValueProperty] = new Binding($"{nameof(Member.Records)}[{discipline.Id}].{nameof(DisciplineRecord.Value)}"),
+                FormatString = "0.0",
+                Increment = 1,
+                BorderBrush = Brushes.Transparent,
+                HorizontalContentAlignment = HorizontalAlignment.Right,
+                VerticalAlignment = VerticalAlignment.Center,
+                ShowButtonSpinner = false
+            }),
+            DisciplineDataType.Time => new FuncDataTemplate<Member>((_, _) => new TimeSpanPicker
+            {
+                [!TimeSpanPicker.TimeSpanProperty] = new Binding($"{nameof(Member.Records)}[{discipline.Id}].{nameof(DisciplineRecord.Value)}")
+            }),
+            _ => throw new FormatException($"Invalid data type {discipline.DataType}")
+        };
+
+        return template;
+    }
+
+    private static string CreateDisciplineColumnTag(DisciplineInfo discipline)
+    {
+        return $"Discipline-{discipline.Id}";
+    }
+
+    private void RemoveDisciplineColumns(IList? disciplines)
+    {
+        if (disciplines is null)
+        {
+            return;
+        }
+
+        foreach (object disciplineObject in disciplines)
+        {
+            if (disciplineObject is DisciplineInfo discipline)
+            {
+                RemoveDisciplineColumn(discipline);
+            }
+        }
+    }
+
+    private void RemoveDisciplineColumn(DisciplineInfo discipline)
+    {
+        string columnTag = CreateDisciplineColumnTag(discipline);
+        IColumn<Member>? disciplineColumn = null;
+        foreach (IColumn<Member> column in TreeDataGridSource.Columns)
+        {
+            if (column.Tag is string tag && tag == columnTag)
+            {
+                disciplineColumn = column;
+                break;
+            }
+        }
+
+        if (disciplineColumn is null)
+        {
+            return;
+        }
+
+        TreeDataGridSource.Columns.Remove(disciplineColumn);
+    }
+
+    private static DockPanel CreateDisciplineColumnHeader(DisciplineInfo discipline)
+    {
+        var panel = new DockPanel
+        {
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            VerticalAlignment = VerticalAlignment.Stretch,
+            Tag = discipline.Id
+        };
+
+        var removeButton = new Button
+        {
+            HorizontalAlignment = HorizontalAlignment.Left,
+            [DockPanel.DockProperty] = Dock.Left,
+            [Attached.IconProperty] = "mdi-close",
+            Margin = new Thickness(0, 0, 5, 0),
+            [ToolTip.TipProperty] = Lang.Resources.InputView_RemoveDiscipline_Button
+        };
+        removeButton.Click += RemoveDiscipline_Button_OnClick;
+        panel.Children.Add(removeButton);
+
+        var iconSort = new Icon
+        {
+            FontSize = 20,
+            HorizontalAlignment = HorizontalAlignment.Right,
+            [DockPanel.DockProperty] = Dock.Right
+        };
+        var iconSortBinding = new Binding
+        {
+            Source = discipline,
+            Path = nameof(DisciplineInfo.SortOrder),
+            Converter = new DisciplineSortToIconConverter()
+        };
+        iconSort.Bind(Icon.ValueProperty, iconSortBinding);
+        panel.Children.Add(iconSort);
+
+        var iconType = new Icon
+        {
+            FontSize = 20,
+            HorizontalAlignment = HorizontalAlignment.Right,
+            [DockPanel.DockProperty] = Dock.Right
+        };
+        var iconTypeBinding = new Binding
+        {
+            Source = discipline,
+            Path = nameof(DisciplineInfo.DataType),
+            Converter = new Converters.DisciplineTypeToIconConverter()
+        };
+        iconType.Bind(Icon.ValueProperty, iconTypeBinding);
+        panel.Children.Add(iconType);
+
+        var text = new TextBlock
+        {
+            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Stretch,
+            Padding = new Thickness(0, 0, 10, 0),
+            [DockPanel.DockProperty] = Dock.Left
+        };
+        var textBinding = new Binding
+        {
+            Source = discipline,
+            Path = nameof(DisciplineInfo.Name)
+        };
+        text.Bind(TextBlock.TextProperty, textBinding);
+        panel.Children.Add(text);
+
+        return panel;
+    }
+
+    private static void RemoveDiscipline_Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not Button button)
+        {
+            return;
+        }
+
+        var panel = button.FindLogicalAncestorOfType<DockPanel>();
+        if (panel is not { DataContext: InputViewModel context, Tag: Guid disciplineId })
+        {
+            return;
+        }
+
+        DisciplineInfo? discipline = context.Data.GetDisciplineById(disciplineId);
+        if (discipline is not null)
+        {
+            context.Data.RemoveDiscipline(discipline);
+        }
+    }
 }
--- TeamSorting/Views/InputView.axaml.cs
+++ TeamSorting/Views/InputView.axaml.cs
@@ -1,71 +1,21 @@
-﻿using System.Collections.Specialized;
-using System.ComponentModel;
-using Avalonia;
+﻿using System.ComponentModel;
 using Avalonia.Controls;
-using Avalonia.Controls.Notifications;
-using Avalonia.Controls.Templates;
-using Avalonia.Data;
+using Avalonia.Controls.Primitives;
 using Avalonia.Input;
 using Avalonia.Interactivity;
-using Avalonia.Layout;
 using Avalonia.LogicalTree;
-using Avalonia.Media;
 using Avalonia.Platform.Storage;
-using Projektanker.Icons.Avalonia;
-using TeamSorting.Controls;
 using TeamSorting.Enums;
 using TeamSorting.Models;
 using TeamSorting.ViewModels;
-using MenuItem = Avalonia.Controls.MenuItem;
 
 namespace TeamSorting.Views;
 
 public partial class InputView : UserControl
 {
-    private WindowNotificationManager? _notificationManager;
-
     public InputView()
     {
         InitializeComponent();
-    }
-
-    protected override void OnInitialized()
-    {
-        if (DataContext is InputViewModel vm)
-        {
-            AddDisciplinesToDataGrid();
-            vm.Data.Disciplines.CollectionChanged += DisciplinesOnCollectionChanged;
-        }
-
-        _notificationManager = new WindowNotificationManager(TopLevel.GetTopLevel(this))
-        {
-            Position = NotificationPosition.BottomRight,
-            Margin = new Thickness(0, 0, 0, 20),
-            MaxItems = 3
-        };
-
-        base.OnInitialized();
-    }
-
-    private void DisciplinesOnCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-    {
-        if (e.Action is NotifyCollectionChangedAction.Remove)
-        {
-            if (e.OldItems is null || e.OldItems.Count <= 0) return;
-            foreach (DisciplineInfo discipline in e.OldItems)
-            {
-                var column = MemberGrid.Columns.First(column =>
-                    column.Tag is DisciplineColumnTag tag && tag.DisciplineId == discipline.Id);
-                MemberGrid.Columns.Remove(column);
-            }
-        }
-        else if (e.Action is NotifyCollectionChangedAction.Reset)
-        {
-            foreach (DataGridColumn? column in MemberGrid.Columns.Where(column => column.Tag is DisciplineColumnTag).ToList())
-            {
-                MemberGrid.Columns.Remove(column);
-            }
-        }
     }
 
     [Localizable(false)]
@@ -106,165 +56,6 @@ public partial class InputView : UserControl
                 await dialog.ShowDialog(mainWindow);
             }
         }
-
-        AddDisciplinesToDataGrid();
-    }
-
-    [Localizable(false)]
-    private void AddDisciplinesToDataGrid()
-    {
-        var context = (InputViewModel)DataContext!;
-
-        foreach (var discipline in context.Data.Disciplines)
-        {
-            if (DataGridContainsDisciplineColumn(MemberGrid, discipline))
-            {
-                continue;
-            }
-
-            DataGridTemplateColumn customColumn = CreateDisciplineColumn(discipline);
-
-            MemberGrid.Columns.Add(customColumn);
-        }
-    }
-
-    private static DataGridTemplateColumn CreateDisciplineColumn(DisciplineInfo discipline)
-    {
-        var customColumn = new DataGridTemplateColumn
-        {
-            Tag = new DisciplineColumnTag(discipline.Id),
-            Header = CreateDisciplineColumnHeader(discipline),
-            IsReadOnly = false,
-        };
-
-        FuncDataTemplate<Member> template;
-        switch (discipline.DataType)
-        {
-            case DisciplineDataType.Number:
-            {
-                template = new FuncDataTemplate<Member>((_, _) =>
-                    new NumericUpDown
-                    {
-                        [!NumericUpDown.ValueProperty] =
-                            new Binding($"{nameof(Member.Records)}[{discipline.Id}].{nameof(DisciplineRecord.Value)}"),
-                        FormatString = "0.0",
-                        Increment = 1,
-                        BorderBrush = Brushes.Transparent,
-                        HorizontalContentAlignment = HorizontalAlignment.Right,
-                        VerticalAlignment = VerticalAlignment.Center,
-                        ShowButtonSpinner = false
-                    });
-                customColumn.CellTemplate = template;
-                break;
-            }
-            case DisciplineDataType.Time:
-            {
-                template = new FuncDataTemplate<Member>((_, _) =>
-                    new TimeSpanPicker
-                    {
-                        [!TimeSpanPicker.TimeSpanProperty] =
-                            new Binding($"{nameof(Member.Records)}[{discipline.Id}].{nameof(DisciplineRecord.Value)}")
-                    });
-                break;
-            }
-            default:
-                throw new FormatException($"Invalid data type {discipline.DataType}");
-        }
-
-        customColumn.CellTemplate = template;
-
-        return customColumn;
-    }
-
-    private bool DataGridContainsDisciplineColumn(DataGrid dataGrid, DisciplineInfo discipline)
-    {
-        var column = dataGrid.Columns.FirstOrDefault(column =>
-            column.Tag is DisciplineColumnTag tag && tag.DisciplineId == discipline.Id);
-        return column is not null;
-    }
-
-    private static DockPanel CreateDisciplineColumnHeader(DisciplineInfo discipline)
-    {
-        var panel = new DockPanel
-        {
-            HorizontalAlignment = HorizontalAlignment.Stretch,
-            VerticalAlignment = VerticalAlignment.Stretch,
-            Tag = discipline.Id
-        };
-
-        var removeButton = new Button()
-        {
-            HorizontalAlignment = HorizontalAlignment.Left,
-            [DockPanel.DockProperty] = Dock.Left,
-            [Attached.IconProperty] = "mdi-close",
-            Margin = new Thickness(0, 0, 5, 0),
-            [ToolTip.TipProperty] = Lang.Resources.InputView_RemoveDiscipline_Button
-        };
-        removeButton.Click += RemoveDiscipline_Button_OnClick;
-        panel.Children.Add(removeButton);
-
-        var iconSort = new Icon
-        {
-            FontSize = 20,
-            HorizontalAlignment = HorizontalAlignment.Right,
-            [DockPanel.DockProperty] = Dock.Right
-        };
-        var iconSortBinding = new Binding
-        {
-            Source = discipline,
-            Path = nameof(DisciplineInfo.SortOrder),
-            Converter = new Converters.DisciplineSortToIconConverter()
-        };
-        iconSort.Bind(Icon.ValueProperty, iconSortBinding);
-        panel.Children.Add(iconSort);
-
-        var iconType = new Icon
-        {
-            FontSize = 20,
-            HorizontalAlignment = HorizontalAlignment.Right,
-            [DockPanel.DockProperty] = Dock.Right
-        };
-        var iconTypeBinding = new Binding
-        {
-            Source = discipline,
-            Path = nameof(DisciplineInfo.DataType),
-            Converter = new Converters.DisciplineTypeToIconConverter()
-        };
-        iconType.Bind(Icon.ValueProperty, iconTypeBinding);
-        panel.Children.Add(iconType);
-
-        var text = new TextBlock
-        {
-            VerticalAlignment = VerticalAlignment.Center,
-            HorizontalAlignment = HorizontalAlignment.Stretch,
-            Padding = new Thickness(0, 0, 10, 0),
-            [DockPanel.DockProperty] = Dock.Left
-        };
-        var textBinding = new Binding
-        {
-            Source = discipline,
-            Path = nameof(DisciplineInfo.Name)
-        };
-        text.Bind(TextBlock.TextProperty, textBinding);
-        panel.Children.Add(text);
-
-        return panel;
-    }
-
-    private static void RemoveDiscipline_Button_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (sender is Button button)
-        {
-            var panel = button.FindLogicalAncestorOfType<DockPanel>();
-            if (panel is { DataContext: InputViewModel context, Tag: Guid disciplineId })
-            {
-                var discipline = context.Data.GetDisciplineById(disciplineId);
-                if (discipline is not null)
-                {
-                    context.Data.RemoveDiscipline(discipline);
-                }
-            }
-        }
     }
 
     private async void SortToTeams_OnClick(object? sender, RoutedEventArgs e)
@@ -303,14 +94,14 @@ public partial class InputView : UserControl
         var member = new Member(context.NewMemberName);
         context.Data.AddMember(member);
         context.NewMemberName = string.Empty;
-        MemberGrid.ScrollIntoView(member, MemberGrid.Columns.First());
+        //TODO: scroll to added member
     }
 
     private void RemoveMemberButton_OnClick(object? sender, RoutedEventArgs e)
     {
         if (sender is Button button)
         {
-            var gridRow = button.FindLogicalAncestorOfType<DataGridRow>();
+            var gridRow = button.FindLogicalAncestorOfType<TreeDataGridRow>();
             if (gridRow?.DataContext is Member member)
             {
                 var context = (InputViewModel)DataContext!;
@@ -345,11 +136,10 @@ public partial class InputView : UserControl
         context.Data.AddDiscipline(discipline);
         //TODO error handling when discipline with same name exists 
         context.NewDisciplineName = string.Empty;
-        AddDisciplinesToDataGrid();
         if (context.Data.Members.Count > 0)
         {
             //TODO scroll even if members are empty
-            MemberGrid.ScrollIntoView(null, MemberGrid.Columns.Last());
+            // MemberGrid.ScrollIntoView(null, MemberGrid.Columns.Last());
         }
     }
 
@@ -381,30 +171,6 @@ public partial class InputView : UserControl
             if (parent?.DataContext is Member member)
             {
                 member.RemoveNotWithMember(removeName);
-            }
-        }
-    }
-
-    private void AddNotWithMemberMenuItem_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (sender is MenuItem { DataContext: Member notWithMember } menuItem)
-        {
-            var row = menuItem.FindLogicalAncestorOfType<DataGridRow>();
-            if (row?.DataContext is Member member)
-            {
-                member.AddNotWithMember(notWithMember);
-            }
-        }
-    }
-
-    private void AddWithMemberMenuItem_OnClick(object? sender, RoutedEventArgs e)
-    {
-        if (sender is MenuItem { DataContext: Member withMember } menuItem)
-        {
-            var row = menuItem.FindLogicalAncestorOfType<DataGridRow>();
-            if (row?.DataContext is Member member)
-            {
-                member.AddWithMember(withMember);
             }
         }
     }
