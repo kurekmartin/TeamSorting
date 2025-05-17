5db8ab63e10450214bad4ccdee4e5e631e995b92
KurekMartin <martinkurek.dev@gmail.com>
2025-05-17 14:40:41 +0200

    Add control for displaying validation errors for TextBlock

---
+++ TeamSorting/Controls/ValidationTextBlock.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+
+namespace TeamSorting.Controls;
+
+public class ValidationTextBlock : TemplatedControl
+{
+    public static readonly StyledProperty<string?> ValidatedTextProperty =
+        TextBlock.TextProperty.AddOwner<ValidationTextBlock>(
+            new StyledPropertyMetadata<string?>(
+                defaultBindingMode: BindingMode.OneWay,
+                enableDataValidation: true));
+
+    public string? ValidatedText
+    {
+        get => GetValue(ValidatedTextProperty);
+        set => SetValue(ValidatedTextProperty, value);
+    }
+
+    protected override void UpdateDataValidation(AvaloniaProperty property, BindingValueType state, Exception? error)
+    {
+        if (property == ValidatedTextProperty)
+        {
+            DataValidationErrors.SetError(this, error);
+        }
+
+        base.UpdateDataValidation(property, state, error);
+    }
+}
